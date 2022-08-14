872f9a1ae3c7e29c586358559d1faeb56ba469ed
Leandro Palma <psychomusicbox@gmail.com>
2022-08-14 16:52:34 -0500

    Logueo de usuarios implementado - v.1.1

--- src/main/java/com/leancoder/ecommerce/controller/LoginController.java
+++ src/main/java/com/leancoder/ecommerce/controller/LoginController.java
@@ -1,10 +1,14 @@
 package com.leancoder.ecommerce.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -15,8 +19,15 @@ public class LoginController {
     private String preToken = "tatitkm";
 
     @GetMapping("/login")
-    public String Login(Model model) {
+    public String Login(@RequestParam(value="error", required = false) String error, Principal principal, RedirectAttributes flash, Model model) {
         model.addAttribute("title", "Iniciar sesion");
+        if (principal != null) {
+            flash.addFlashAttribute("info", "Ya se ha iniciado sesion.");
+            return "redirect:/";
+        }
+        if (error != null) {
+            model.addAttribute("errorLogin", "No se encontro una cuenta con los credenciales ingresados.");
+        }
         return "login";
     }
 
